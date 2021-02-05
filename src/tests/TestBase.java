786a2a9644d38b0665971bf9168a4b1a2478c051
MarijanVilis <marijanvilis2@gmail.com>
2021-02-05 21:50:59 +0100

    Infrastructure for tests added

---
+++ src/tests/TestBase.java
@@ -0,0 +1,29 @@
+package tests;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class TestBase {
+	
+	WebDriver driver;
+	
+	@BeforeClass
+	public void preKlase() throws IOException {
+		System.setProperty("webdriver.chrome.driver",
+				"driver-lib\\chromedriver.exe");
+		
+		this.driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	
+	@AfterClass
+	public void pposleKlase() throws IOException {
+		driver.close();
+	}
+	
+	
+}
